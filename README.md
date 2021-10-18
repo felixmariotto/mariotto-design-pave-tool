b22cd01ee02d8cb88312dce4b50058673789cc8b
Felix Mariotto <felixmariotto@gmail.com>
2021-10-18 14:23:37 +0200

    create root file

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# rhino-awesome-pave
+# rhino_awesome_pave
 a set of tools to help you model gemstones pave settings in Rhino.
---
+++ rhino_awesome_pave.py
@@ -0,0 +1,8 @@
+
+import mainui
+
+def rhino_awesome_pave():
+    print('ok')
+
+if __name__ == '__main__':
+    rhino_awesome_pave()
